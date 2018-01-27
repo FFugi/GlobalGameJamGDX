454c9297c1961b73f954e0b29c1797d4aea894cc
FFugi <fudifil@gmail.com>
2018-01-27 20:55:47 +0100

    Added particles managing

--- core/src/com/mygdx/game/SoundParticle.java
+++ core/src/com/mygdx/game/SoundParticle.java
@@ -12,70 +12,61 @@ import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.Timer;
 
 public class SoundParticle extends GameObject {
-    float x, y;
-    Vector2 startPos;
-    Body body;
-    float lifeTime;
-    float scaleSize=1;
+	float x, y;
+	Vector2 startPos;
+	Body body;
 
-    public SoundParticle(Vector2 position, Vector2 velocity, World world) {
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.DynamicBody;
-        bodyDef.position.set(position);
-        bodyDef.linearVelocity.set(velocity);
-        FixtureDef fixture = new FixtureDef();
-        fixture.shape = new CircleShape();
-        fixture.shape.setRadius(1);
-        fixture.filter.categoryBits = 2;
-        fixture.filter.maskBits = 1;
-        body = world.createBody(bodyDef);
-        body.createFixture(fixture);
-        body.setUserData(this);
-        startPos=new Vector2();
-        startPos.x=position.x;
-        startPos.y=position.y;
-        
-    }
+	float scaleSize = 1;
 
-    @Override
-    public void Draw(SpriteBatch batch) {
-        System.out.println("drawing");
-    }
-    
-    public void Draw(ShapeRenderer shapeRenderer) {
-    	float radius=0;
-    	if(body.getLinearVelocity().x==0 && body.getLinearVelocity().y==0) {
-    		radius=scaleSize;
-    		scaleSize+=Gdx.graphics.getDeltaTime()*3;
-    	}
-    	else {
-    		radius=1;
-    	}
-    	
-    	shapeRenderer.begin(ShapeType.Filled);
-    	shapeRenderer.setColor(0f, 1f-lifeTime/5, 0f, 0f);
-    	shapeRenderer.circle(body.getPosition().x, body.getPosition().y, radius);
-    	shapeRenderer.end();
-    }
+	public SoundParticle(Vector2 position, Vector2 velocity, World world) {
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.type = BodyDef.BodyType.DynamicBody;
+		bodyDef.position.set(position);
+		bodyDef.linearVelocity.set(velocity);
+		FixtureDef fixture = new FixtureDef();
+		fixture.shape = new CircleShape();
+		fixture.shape.setRadius(1);
+		fixture.filter.categoryBits = 2;
+		fixture.filter.maskBits = 1;
+		body = world.createBody(bodyDef);
+		body.createFixture(fixture);
+		body.setUserData(this);
+		startPos = new Vector2();
+		startPos.x = position.x;
+		startPos.y = position.y;
+	}
 
-    public static void emit(Vector2 position, int amount, float speed, World world, List<SoundParticle> particles) {
-       for (int i = 0; i < amount; i++) {
-            Vector2 velocity = new Vector2(1, 1).scl(speed).rotate(360.0f / amount * i);
-            particles.add(new SoundParticle(position, velocity, world));
-       }
-    }
-    
-    public float GetDistance() {
-    	return startPos.dst(body.getPosition());
-    }
-    
-    public float GetLifeTime() {
-    	lifeTime+=Gdx.graphics.getDeltaTime();
-    	return lifeTime;
-    }
+	@Override
+	public void Draw(SpriteBatch batch) {
+		System.out.println("drawing");
+	}
+
+	public void Draw(ShapeRenderer shapeRenderer, float progress) {
+		float radius = 0;
+		if (body.getLinearVelocity().x == 0 && body.getLinearVelocity().y == 0) {
+			radius = 3+ scaleSize;
+			scaleSize += Gdx.graphics.getDeltaTime() * 2;
+		} else {
+			radius = 3;
+		}
+		shapeRenderer.begin(ShapeType.Filled);
+		shapeRenderer.setColor(0f, 1f-progress, 0f, 0f);
+		shapeRenderer.circle(body.getPosition().x, body.getPosition().y, radius);
+		shapeRenderer.end();
+	}
+
+
+	public float GetDistance() {
+		return startPos.dst(body.getPosition());
+	}
+
 
 	public void DestroyBody() {
 		body.getWorld().destroyBody(body);
-		body=null;
+		body = null;
+	}
+
+	public Body getBody() {
+		return body;
 	}
 }
