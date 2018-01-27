8d4d61811e21fa14ea9bd157844a890d973eed0c
SzymonZebrowski <szymekze@gmail.com>
2018-01-27 15:05:55 +0100

    Add terrain scale and rotation

--- core/src/com/mygdx/game/SoundEmitter.java
+++ core/src/com/mygdx/game/SoundEmitter.java
@@ -1,6 +1,7 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
@@ -10,23 +11,30 @@ class SoundEmitter {
 
     private float maxAge;
     private float age;
+    private float radius;
     private World world;
     private Vector2 position;
 
-    public SoundEmitter(Vector2 position, World world) {
+    public SoundEmitter(Vector2 position, World world, float maxAge, float radius) {
        this.position = position;
        this.world = world;
+       this.maxAge = maxAge;
+       this.radius = radius;
        age = 0f;
-       maxAge = 10f;
     }
 
     public void Draw(ShapeRenderer renderer) {
        age += Gdx.graphics.getDeltaTime();
+       Gdx.gl.glEnable(GL30.GL_BLEND);
        renderer.begin(ShapeRenderer.ShapeType.Filled);
        float green = (maxAge - age) / maxAge * 1.0f;
        renderer.setColor(0, green, 0, 1);
-       renderer.circle(position.x, position.y, 10 * age);
+       renderer.circle(position.x, position.y, radius * age / maxAge);
+        renderer.setColor(0, 0, 0, 0.25f);
+        renderer.circle(position.x, position.y, 0.95f*(radius * age / maxAge));
        renderer.end();
+       Gdx.gl.glDisable(GL30.GL_BLEND);
+
     }
 
     public boolean isDead() {
